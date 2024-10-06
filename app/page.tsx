587502caff73e8277a7fa6f592118c2c73d2b5e4
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-06 18:18:59 +0200

    ADD: Main page (todo styles)

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 import { Button } from "@/components/ui/button";
+
+
 
 export default function Home() {
   return (
     <>
-      <main>
+      {/* TODO: Styles of the main page */}
+      <main className="h-screen overflow-auto no-scrollbar flex justify-center items-center">
+        {/* App title */}
         <h1>Hello Axel !!</h1>
-        <Button>Click me</Button>
+
+        {/* Loading image */}
+        <img src="" alt="" />
+
+        {/* Button to enter registration page */}
+        <Link href="/register-page">
+          <Button>Register</Button>
+        </Link>
       </main>
     </>
   );
