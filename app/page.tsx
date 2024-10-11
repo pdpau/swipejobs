771c17f599ca571fd3c7d6e138b219fe66d0691d
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-11 13:53:19 +0200

    UPDATE: init and end page structure done

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
+    /* TODO: Responsive */
+    /* TODO: Escollir tipus de lletra pel projecte */
+
     <>
-      {/* TODO: Styles of the main page */}
-      <main className="h-screen overflow-auto no-scrollbar flex justify-center items-center">
-        {/* App title */}
-        <h1>Hello Axel !!</h1>
+      <main className="flex flex-col items-center h-screen overflow-auto no-scrollbar">
+        <div
+          className={cn(
+            "h-screen w-1/2 p-4", 
+            "flex flex-col justify-center", 
+            "bg-white"
+          )}
+        >
+          <h1 className="text-center text-6xl font-bold">
+            #SwipeTalentYouz
+          </h1>
 
-        {/* Loading image */}
-        <img src="" alt="" />
+          <img src="" alt="" /> {/* TODO: Imatge del estil del logo */}
 
-        {/* Button to enter registration page */}
-        <Link href="/register-page">
-          <Button>Register</Button>
-        </Link>
+          <div className="text-center mt-6"> {/* TODO: Button styles */}
+            <Link href="/register-page">
+              <Button className="">
+                ¿Registra't o Començar?
+              </Button>
+            </Link>
+          </div>
+        </div>
       </main>
     </>
   );
