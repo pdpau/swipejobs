e49e3ef93130f650d2bdc8d6951c27197ba348ab
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-11 15:21:49 +0200

    ADD: Some styles to register page

--- app/register-page/page.tsx
+++ app/register-page/page.tsx
@@ -8,11 +8,9 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-/* Props */
-type Props = {
-  
-};
+import { Separator } from "@/components/ui/separator";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 
 
 /* Main component */
@@ -23,6 +21,9 @@ export default function RegisterPage() {
   const [surname, setSurname] = useState("");
   const [email, setEmail] = useState("");
   const [tlf, setTlf] = useState("");
+  const [conditionsAccepted, setConditionsAccepted] = useState(false);
+
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   /* Functions */
   const handleRegister = () => {
@@ -38,70 +39,162 @@ export default function RegisterPage() {
     console.log("Tlf:", tlf);
   };
 
+
   return (
     /* TODO: Responsive */
-    <main className={cn("h-screen w-screen my-10", "flex justify-center items-center")}>
-      <div id="register-main-div" className={cn("w-1/3 h-2/3 rounded-3xl bg-[#f2f2f0]", "flex flex-col justify-center items-center space-y-5")}>
+
+    <main className={cn("h-screen w-screen", "flex justify-center items-center", "bg-black")}>
+      <div id="register-main-div" className={cn("w-full max-w-md h-3/4", "flex flex-col justify-center items-center space-y-8", "text-white")}>
         {/* Title */}
-        <div className="w-2/3 my-6 flex justify-center">
-          <h1 className="font-bold text-2xl">Introduce tus datos</h1>
-        </div>
+        <h1 className="text-4xl font-bold text-center text-white"> {/* TODO: Youz style */}
+          #SwipeTalent<span className="text-[#A4FF00]">Youz</span>
+        </h1>
 
         {/* Form */}
-        <div className="w-2/3">
+        <div className="w-4/5 space-y-1">
           <Input 
-            className="bg-[#c9c9c9] text-muted-foreground"
             type="text"
             name="name"
-            placeholder="Nombre"
+            placeholder="NOM"
             value={name}
+            autoComplete="off"
+            className={cn(
+              "bg-transparent text-white placeholder-white border-0", 
+              "focus-visible:ring-offset-0 focus-visible:ring-0",
+              ""
+            )}
             onChange={(e) => setName(e.target.value)}
           />
-        </div>
-        <div className="w-2/3">
-          <Input 
-            className="bg-[#c9c9c9] text-muted-foreground"
+          <Separator />
+          <Input
             type="text"
-            name="surname"
-            placeholder="Apellido"
+            name="name"
+            placeholder="COGNOMS"
             value={surname}
-            onChange={(e) => setSurname(e.target.value)}
+            autoComplete="off"
+            className={cn(
+              "bg-transparent text-white placeholder-white border-0", 
+              "focus-visible:ring-offset-0 focus-visible:ring-0",
+              ""
+            )}
+            onChange={(e) => setName(e.target.value)}
           />
-        </div>
-        <div className="w-2/3">
-          <Input 
-            className="bg-[#c9c9c9] text-muted-foreground"
+          <Separator />
+          <Input
             type="email"
             name="email"
-            placeholder="Email"
+            placeholder="EMAIL"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="off"
+            className={cn(
+              "bg-transparent text-white placeholder-white border-0", 
+              "focus-visible:ring-offset-0 focus-visible:ring-0",
+              ""
+            )}
+            onChange={(e) => setName(e.target.value)}
           />
-        </div>
-        <div className="w-2/3">
-          <Input 
-            className="bg-[#c9c9c9] text-muted-foreground"
+          <Separator />
+          <Input
             type="tel"
             name="tlf"
-            placeholder="Teléfono"
+            placeholder="TELÈFON"
             value={tlf}
-            onChange={(e) => setTlf(e.target.value)}
+            autoComplete="off"
+            className={cn(
+              "bg-transparent text-white placeholder-white border-0", 
+              "focus-visible:ring-offset-0 focus-visible:ring-0",
+              ""
+            )}
+            onChange={(e) => setName(e.target.value)}
           />
+          <Separator />
         </div>
 
         {/* TODO: Aceptar condiciones */}
+        <div className="w-4/5 flex items-start"> {/* TODO: Quadrar a dins del width del formulari */}
+          <Checkbox 
+            id="accept-conditions"
+            checked={conditionsAccepted}
+            onCheckedChange={() => setConditionsAccepted(!conditionsAccepted)}
+            className="w-5 h-5 mr-2 text-[#A4FF00] bg-transparent border border-white"
+          />
+          <label htmlFor="accept-conditions" className="text-sm text-white">
+            Accepto les condicions d'ús i la política de privacitat
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+              <DialogTrigger asChild>
+                <button className="underline text-[#A4FF00]">
+                  Termes i condicions
+                </button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Termes i condicions</DialogTitle>
+                  <DialogDescription>
+                    Aquests són els termes i condicions que has d'acceptar per poder continuar amb el registre.
+                    <ul className="list-disc pl-4 mt-4">
+                      <li>Termes i condició 1</li>
+                      <li>Termes i condició 2</li>
+                      <li>Termes i condició 3</li>
+                    </ul>
+                  </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                  <Button 
+                    className="bg-[#A4FF00] text-black"
+                    onClick={() => {setConditionsAccepted(true); setDialogOpen(false);}}
+                  >
+                    Acceptar
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </label>
+        </div>
 
 
         {/* Submit button */}
-        <div className="w-2/3 flex justify-center">
-          {/* TODO: If all info not complete do not link to next page */}
-          <Link href="/offers-page">
-            <Button type="submit" onClick={handleRegister} className="my-4 bg-[#ff6725] text-black">
+        <div className="w-4/5 flex justify-center">
+          <Link href={conditionsAccepted && name && surname && email && tlf ? "/offers-page" : "#"}>
+            <Button 
+              type="submit" 
+              onClick={handleRegister} 
+              className={cn(
+                "w-full py-3 rounded-full",
+                "bg-[#A4FF00] hover:bg-[#8FE000] active:bg-[#6CC700]",
+                "text-black font-bold text-lg",
+                conditionsAccepted && name && surname && email && tlf ? "cursor-pointer" : "cursor-not-allowed opacity-50"
+              )}
+              /* disabled={!conditionsAccepted || !name || !surname || !email || !tlf} */
+            >
               ¿Registrar datos?
             </Button>
           </Link>
         </div>
+
+        {/* TODO: Footer (logo vibez) */}
+        <div className="mt-10">
+          {/* <p className="text-sm text-center text-white">© 2024 Nom de la empresa. Tots els drets reservats.</p> */}
+        </div>
       </div>
+
+      {/* TODO: Background waves */}
+      {/* TODO: ¿¿¿Totes les pagines amb aquest background??? */}
+      <div className="absolute top-0 left-0 w-full h-1/6 bg-gradient-to-t from-transparent to-pink-500">
+        {/* Usar pseudo-elemento para la ola */}
+        <div className="wave"></div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full h-1/6 bg-gradient-to-b from-transparent to-pink-500">
+        {/* Usar pseudo-elemento para la ola */}
+        <div className="wave"></div>
+      </div>
+      <style jsx>
+        {`
+          .wave {
+            /* TODO: Make it a waving wave */
+
+          }
+        `}
+      </style>
     </main>
   );
 };
