0ee9d6ddb9612beec7a5bce79cc09d68e53cc27e
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-11 12:32:18 +0200

    INIT thank you page

---
+++ app/thankyou-page/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+/* Imports */
+import { useState } from "react";
+
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+/* Props */
+type Props = {
+  
+};
+
+
+/* Main component */
+export default function RegisterPage() {
+
+  /* Variables */
+
+  /* Functions */
+
+  return (
+    <main className={cn("")}>
+      <p>Thank you</p>
+    </main>
+  );
+};
