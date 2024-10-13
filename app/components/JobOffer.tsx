3a70b018e137a2985609538042adc518a4f1940c
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-13 21:34:11 +0200

    UPDATE: Offers from DB OK

--- app/components/JobOffer.tsx
+++ app/components/JobOffer.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-
 /* Imports */
+import { useEffect, useState } from "react";
+
 import TinderCard from "react-tinder-card";
 
 import { OfferType } from "../types";
@@ -11,26 +12,54 @@ import { Separator } from "@/components/ui/separator";
 
 /* Props */
 type Props = {
-    offer: OfferType;
+    dbOffer: any;
     onSwipe: (direction: string) => void;
 };
 
+const transformDataToOfferType = (dbData: any): OfferType | null => {
+    if (!dbData) return null;
+    console.log("DB Data:", dbData);
+    return {
+        id: dbData.id || 0,
+        companyName: dbData.companyName || "Unknown Company", 
+        title: dbData.title || "No title provided",
+        location: dbData.location || "No location provided",
+        schedule: dbData.schedule || "No schedule provided",
+        contractType: dbData.contractType || "Unknown contract type",
+        description: dbData.description || "No description available",
+        
+        // Accedemos directamente a los arrays
+        functions: dbData.functions.functions || ["No functions provided"],
+        requirements: dbData.requirements.requirements || ["No requirements provided"]
+    };
+}
+
+
 /* Main component */
-export function JobOffer({ offer, onSwipe }: Props) {
+export function JobOffer({ dbOffer, onSwipe }: Props) {
+    /* Variables */
+    const [offer, setOffer] = useState<OfferType | null>(null);
 
     /* Functions */
-    
+    useEffect(() => {
+        const mappedOffer = transformDataToOfferType(dbOffer);
+        setOffer(mappedOffer);
+        console.log("Offer:", mappedOffer);
+    }, [dbOffer]);
 
+    if (!offer) return <div>Loading...</div>;
     return (
         /* TODO: Acabar styles i fer responsive per mòbil */
         /* TODO: ¿¿Fer els div com funcriones y requisitos?? */
         /* TODO: Canviar paleta de colors (fer variables dels colors) */
         /* TODO: Escollir tipus de lletra pel projecte */
 
+        /* TODO: Ajustar els max-w i max-h del TinderCard per optimitzar a tots els dispositius */
+
         <TinderCard
             className={cn(
                 "flex justify-center items-center", 
-                "w-2/3 h-4/5", 
+                "w-5/6 h-2/3 max-w-[400px] max-h-[900px]", 
                 "bg-slate-950 rounded-3xl shadow-xl",
                 ""
             )}
@@ -38,54 +67,56 @@ export function JobOffer({ offer, onSwipe }: Props) {
             onSwipe={onSwipe}
             preventSwipe={["up", "down"]}
         >
-            <div className="h-full p-6 flex flex-col justify-between text-white">
+            <div className="h-full px-4 py-2 flex flex-col justify-between text-white"> {/* TODO: Escollir padding */}
                 {/* Logo o nombre de la empresa */}
-                <div className="h-[15%] flex flex-col items-center">
-                    <h2 className="text-6xl font-bold">{offer.companyName}</h2>
-                    <p className="text-2xl text-pink-400 my-3">{offer.title}</p>
+                <div className="h-[12%] mb-2 flex flex-col items-center">
+                    <h2 className="text-2xl font-bold">{offer.companyName}</h2>
+                    <p className="text-lg text-pink-400 my-1">{offer.title}</p>
                 </div>
-
-                <Separator className="bg-pink-500"/>
 
                 {/* Detalles de la oferta */}
-                <div className="h-[5%] flex justify-around items-center text-lg my-2">
-                    <div className="flex flex-col items-center">
-                        <p><strong><u>Ubicació</u></strong></p>
-                        <p>{offer.location}</p>
+                <div className="h-[8%] mb-2 flex justify-around items-center text-sm">
+                    <div className="w-1/3 h-full mr-1 flex items-center justify-center bg-slate-800 rounded-xl overflow-auto no-scrollbar">
+                        <div className="flex flex-col items-center">
+                            <p><strong><u>Ubicació</u></strong></p>
+                            <p>{offer.location}</p>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <p><strong>Horari:</strong></p>
-                        <p>{offer.schedule}</p>
+                    <div className="w-1/3 h-full mx-1 flex items-center justify-center bg-slate-700 rounded-xl overflow-auto no-scrollbar">
+                        <div className="flex flex-col items-center">
+                            <p><strong>Contracte</strong></p>
+                            <p>{offer.contractType}</p>
+                        </div>
                     </div>
-                    <div className="flex flex-col items-center">
-                        <p><strong>Tipus de contracte</strong></p>
-                        <p>{offer.contractType}</p>
+                    <div className="w-1/3 h-full ml-1 flex items-center justify-center bg-slate-800 rounded-xl overflow-auto no-scrollbar">
+                        <div className="flex flex-col items-center">
+                            <p><strong>Horari:</strong></p>
+                            <p>{offer.schedule}</p>
+                        </div>
                     </div>
                 </div>
-
-                <Separator className="bg-pink-500"/>
 
                 {/* Descripción de la empresa y la oferta */}
-                <div className="h-[40%] mb-4 px-4">
-                    <h3 className="text-pink-400 text-2xl my-2">Descripció</h3>
-                    <p className="text-lg">{offer.description}</p>
+                <div className="h-[35%] mb-2 flex">
+                    <div className="w-full px-4 bg-slate-600 rounded-xl overflow-auto no-scrollbar">
+                        <h3 className="text-pink-400 text-lg my-1">Descripció</h3>
+                        <p className="text-xs">{offer.description}</p>
+                    </div>
                 </div>
 
-                <Separator className="bg-pink-500"/>
-
                 {/* Funciones y Requisitos */}
-                <div className="h-[35%] flex justify-between">
-                    <div className="w-1/2 mr-2 my-2 px-4 bg-slate-800 rounded-2xl">
-                        <h3 className="text-pink-400 text-2xl my-2">Funcions</h3>
-                        <ul className="list-disc pl-5 text-lg">
+                <div className="h-[45%] mb-2 flex justify-between">
+                    <div className="w-1/2 mr-1 px-4 bg-slate-800 rounded-xl overflow-auto no-scrollbar">
+                        <h3 className="text-pink-400 text-lg my-2">Funcions</h3>
+                        <ul className="list-disc pl-5 text-xs">
                             {offer.functions.map((func, index) => (
                                 <li key={index}>{func}</li>
                             ))}
                         </ul>
                     </div>
-                    <div className="w-1/2 ml-2 my-2 px-4 bg-slate-700 rounded-2xl">
-                        <h3 className="text-pink-400 text-2xl my-2">Requisits</h3>
-                        <ul className="list-disc pl-5 text-lg">
+                    <div className="w-1/2 ml-1 px-4 bg-slate-700 rounded-xl overflow-auto no-scrollbar">
+                        <h3 className="text-pink-400 text-lg my-2">Requisits</h3>
+                        <ul className="list-disc pl-5 text-xs">
                             {offer.requirements.map((req, index) => (
                                 <li key={index}>{req}</li>
                             ))}
@@ -93,15 +124,14 @@ export function JobOffer({ offer, onSwipe }: Props) {
                     </div>
                 </div>
 
-                <Separator className="bg-pink-500"/>
-
                 {/* Indicadores "like" o "pass" */}
-                <div className="h-[5%]"> {/* TODO: Canviar els botons per algo guapo */}
+                {/* TODO: Canviar els botons per algo guapo */}
+                {/* <div className="h-[5%]">
                     <div className="flex justify-between mt-4">
                         <button className="bg-pink-400 text-white px-4 py-2 rounded-lg">Pass</button>
                         <button className="bg-pink-400 text-white px-4 py-2 rounded-lg">Like</button>
                     </div>
-                </div>
+                </div> */}
             </div>
         </TinderCard>
     );
