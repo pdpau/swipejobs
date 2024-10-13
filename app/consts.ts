7b217fd296e2af6bc574e5b42a92df95f6ad5d54
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-13 20:46:42 +0200

    Mock offers into consts file

---
+++ app/consts.ts
@@ -0,0 +1,103 @@
+
+
+
+
+
+/* Mock offers */
+export const listOfOffersExample = [
+    {
+        id: 1,
+        companyName: "Google",
+        title: "Frontend Developer",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    },
+    {
+        id: 2,
+        companyName: "Amazon",
+        title: "Backend Developer",
+        location: "Madrid",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    },
+    {
+        id: 3,
+        companyName: "Apple",
+        title: "Fullstack Developer",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    },
+    {
+        id: 4,
+        companyName: "Microsoft",
+        title: "DevOps Engineer",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    },
+    {
+        id: 5,
+        companyName: "Facebook",
+        title: "Data Scientist",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    }
+] as const;
