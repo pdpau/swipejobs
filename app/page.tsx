3a16630b8f4c9ff335ee439b73b9e79e6a44cdae
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-18 00:22:23 +0200

    main page done (falta estils botó)

--- app/page.tsx
+++ app/page.tsx
@@ -4,36 +4,45 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
+import Vibez from "./public/vibez.svg";
+import VibezFestival from "./public/vibez_festival.svg";
+import LogoZ from "./public/logo_z.svg";
+//import Youz from "./public/youz.svg";
+
+/* Colors of the event logo */
+// Green: #90FF23 / rgba(144,255,35,255) / vibezgreen-400
+// Pink: #FF01FC / rgba(255,1,252,255) / vibezpink-600
 
 export default function Home() {
   return (
-    /* TODO: Responsive */
-    /* TODO: Escollir tipus de lletra pel projecte */
-
     <>
-      <main className="flex flex-col items-center h-screen overflow-auto no-scrollbar">
-        <div
-          className={cn(
-            "h-screen w-1/2 p-4", 
-            "flex flex-col justify-center", 
-            "bg-white"
-          )}
-        >
-          <h1 className="text-center text-6xl font-bold">
-            #SwipeTalentYouz
-          </h1>
-
-          <img src="" alt="" /> {/* TODO: Imatge del estil del logo */}
-
+      <main 
+        className={cn(
+          "h-screen w-screen", 
+          "flex justify-center items-center", 
+          "bg-slate-100", 
+          "overflow-auto no-scrollbar"
+        )}
+      >
+        <VibezFestival className="absolute top-20 left-1/2 transform -translate-x-1/2 h-28 w-28" />
+        {/* <Vibez className="absolute top-20 left-1/2 transform -translate-x-1/2 h-24 w-24" /> */}
+        {/* <LogoZ className="absolute top-20 left-1/2 transform -translate-x-1/2 h-24 w-24" /> */}
+        <div className={cn("h-full w-5/6", "flex flex-col justify-center items-center", "space-y-6")}>
+          <h3 className="text-center text-4xl font-extrabold text-slate-900">
+            FES MATCH AMB OFERTES DE TREBALL
+          </h3>
+          <p className="text-center text-xl text-slate-700">
+            Després del registre veuràs una sèrie d'ofertes de treball, tria les que més t'agradin i t'enviarem el recull.
+          </p>
           <div className="text-center mt-6"> {/* TODO: Button styles */}
             <Link href="/register-page">
-              <Button className="">
-                ¿Registra't o Començar?
-              </Button>
+              <button className="text-lg font-semibold text-slate-900 rounded-lg px-4 py-2 shadow-md bg-vibezgreen-400 hover:bg-vibezgreen-600">
+                Començar
+              </button>
             </Link>
           </div>
         </div>
       </main>
     </>
   );
-}
+};
