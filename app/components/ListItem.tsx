595a1db48922be64b7946f20c2b2ddb22d202f74
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-17 18:15:24 +0200

    UPDATE: Line clamps

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -12,11 +12,10 @@ type Props = {
 };
 
 export function ListItem({ item, color }: Props) {
-    /* TODO: Style each item */
     return (
         <li className="flex items-center space-x-2 py-1">
-            <FaCheckCircle className={color} /> {/* Icono antes del texto */}
-            <span className="text-gray-200 text-base">{item}</span>
+            <FaCheckCircle className={color} /> {/* TODO: Cambiar icono antes del texto */}
+            <span className="text-gray-200 text-lg font-medium">{item}</span>
         </li>
     )
 };
