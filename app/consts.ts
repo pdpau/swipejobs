1b7d0e0f6f3c358d29656caa2df00a724381d5ca
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-18 14:18:25 +0200

    more mock offers

--- app/consts.ts
+++ app/consts.ts
@@ -100,5 +100,43 @@ export const listOfOffersExample = [
             "Coneixements de ReactJS",
             "Coneixements de HTML, CSS i JavaScript"
         ]
+    },
+    {
+        id: 6,
+        companyName: "Twitter",
+        title: "Product Manager",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
+    },
+    {
+        id: 7,
+        companyName: "LinkedIn",
+        title: "UX/UI Designer",
+        location: "Barcelona",
+        schedule: "Full-time",
+        contractType: "Indefinite",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nisl nec sem aliquam ultricies. Nulla facilisi. Proin auctor, libero et ultricies aliquet, nunc libero tincidunt libero, nec ultricies libero nunc nec libero.",
+        functions: [
+            "Desenvolupament de la interfície d'usuari",
+            "Optimització de la velocitat de càrrega",
+            "Implementació de noves funcionalitats"
+        ],
+        requirements: [
+            "Experiència prèvia en desenvolupament web",
+            "Coneixements de ReactJS",
+            "Coneixements de HTML, CSS i JavaScript"
+        ]
     }
 ] as OfferType[];
