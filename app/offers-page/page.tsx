1bd71c5369cac8e1187175ec3f0ecf4044626017
pdpau <56670577+pdpau@users.noreply.github.com>
2024-10-20 18:08:21 +0200

    swipe icons

--- app/offers-page/page.tsx
+++ app/offers-page/page.tsx
@@ -10,10 +10,13 @@ import { cn } from '@/lib/utils';
 
 import { JobOffer } from '../components/JobOffer';
 import { JobOffer2 } from '../components/JobOffer2';
+import { JobOffer3 } from '../components/JobOffer3';
 import { PaginationDots } from '../components/PaginationDots';
 
 import { OfferType } from "../types";
 import { listOfOffersExample } from "../consts";
+
+import { TiTick, TiTimes } from 'react-icons/ti';
 
 /* Import logos */
 import LogoZ from "../public/logo_z.svg";
@@ -29,6 +32,19 @@ export default function OffersPage() {
   const [userId, setUserId] = useState<number | null>(null); // userId as number or null
   const [loading, setLoading] = useState(true); // Estado para mostrar un loader si es necesario
   const [currentIndex, setCurrentIndex] = useState(0); // Index de la oferta actual
+
+  /* Swipeable */
+  const [isSwiping, setIsSwiping] = useState(false);
+  const [lastSwipe, setLastSwipe] = useState<string | null>(null);
+  const handleSwipeStart = (direction: string) => {
+    setIsSwiping(true);
+    setLastSwipe(direction);
+  };
+  const handleSwipeEnd = () => {
+    setIsSwiping(false);
+    setLastSwipe(null);
+  };
+  /* End of swipeable */
 
   /* Functions */
   const fetchOffers = async () => {
@@ -97,11 +113,37 @@ export default function OffersPage() {
         currentIndex !== null && currentIndex >= 0 && (
           <div className="w-screen h-screen flex flex-col justify-center items-center">
             {/* <JobOffer dbOffer={offersBD[currentIndex]} onSwipe={onSwipe} /> */}
-            <JobOffer2 dbOffer={offersBD[currentIndex]} onSwipe={onSwipe} />
+            {/* <JobOffer2 dbOffer={offersBD[currentIndex]} onSwipe={onSwipe} /> */}
+            <JobOffer3 
+              dbOffer={offersBD[currentIndex]}
+              onSwipe={onSwipe}
+              onSwipeStart={handleSwipeStart}
+              onSwipeEnd={handleSwipeEnd}
+              isSwiping={isSwiping}
+              lastSwipe={lastSwipe}
+            />
             <PaginationDots totalOffers={offersBD.length} currentIndex={currentIndex} />
           </div>
         )
       )}
+
+      {/* Icono de like */}
+      <div
+        className={`absolute right-[3%] top-[81%] transform -translate-y-1/2 transition-transform duration-500 ease-in-out ${
+          isSwiping && lastSwipe === 'right' ? '-translate-x-[40vw] -translate-y-[40vh] scale-300' : 'scale-100'
+        }`}
+      >
+        <TiTick className="text-green-500" size={60} />
+      </div>
+      {/* Icono de dislike */}
+      <div
+        className={`absolute left-[3%] top-[81%] transform -translate-y-1/2 transition-transform duration-500 ease-in-out ${
+          isSwiping && lastSwipe === 'left' ? 'translate-x-[40vw] -translate-y-[40vh] scale-300' : 'scale-100'
+        }`}
+      >
+        <TiTimes className="text-red-500" size={60} />
+      </div>
+
     </main>
   );
 };
